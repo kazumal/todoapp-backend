d7b306fcaf578c45aa4b91403eb4a2e9d5ac52fe
kazumal <kazuma.hamamoto12@gmail.com>
2024-12-24 18:58:20 +0900

    [追加]認可済みUserのみtaskAPIを使用できるように修正

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -6,35 +6,42 @@ import {
   Delete,
   Param,
   Body,
+  UseGuards,
 } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './entities/task.entity';
 import { TaskBody } from '../interface/task.dto';
+import { AuthGuard } from '@nestjs/passport';
 @Controller('tasks')
 export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
 
   @Get()
+  @UseGuards(AuthGuard('jwt'))
   findAll(): Promise<Task[]> {
     return this.tasksService.findAll();
   }
 
   @Get(':id')
+  @UseGuards(AuthGuard('jwt'))
   findOne(@Param('id') id: string): Promise<Task> {
     return this.tasksService.findOne(id);
   }
 
   @Post()
+  @UseGuards(AuthGuard('jwt'))
   create(@Body() task: TaskBody): Promise<Task> {
     return this.tasksService.create(task);
   }
 
   @Put(':id')
+  @UseGuards(AuthGuard('jwt'))
   update(@Param('id') id: string, @Body() task: TaskBody): Promise<Task> {
     return this.tasksService.update(id, task);
   }
 
   @Delete(':id')
+  @UseGuards(AuthGuard('jwt'))
   delete(@Param('id') id: string): Promise<void> {
     return this.tasksService.delete(id);
   }
