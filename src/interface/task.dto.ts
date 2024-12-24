34708de09d3ad54c3c784e3d79415f13e9744eff
kazumal <kazuma.hamamoto12@gmail.com>
2024-12-24 13:49:52 +0900

    [追加]パラメータバリデーションの追加

--- src/interface/task.dto.ts
+++ src/interface/task.dto.ts
@@ -1,5 +1,12 @@
-export class TaskDto {
+import { IsNotEmpty, IsOptional, IsBoolean } from 'class-validator';
+
+export class TaskBody {
+  @IsNotEmpty()
   title: string;
+
+  @IsOptional()
   description?: string;
+
+  @IsBoolean()
   isCompleted: boolean;
 }
